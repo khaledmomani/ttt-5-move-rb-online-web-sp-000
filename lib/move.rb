6060be95a5c68ee13a1815459e2e5f78190ff494
Khaled S Momani <khaledmomani4@gmail.com>
2020-08-20 23:06:53 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ def input_to_index(user_input = [1,2,3,4,5,6,7,8,9])
   user_input.to_i - 1
 end
 
-def move(board = ["1","2","3","4","5","6","7","8","9"]
+def move(board = ["1","2","3","4","5","6","7","8","9"])
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
