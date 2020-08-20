f5243aa13a7e01c4acaacbff124af9c0bd6c5e60
Khaled S Momani <khaledmomani4@gmail.com>
2020-08-20 23:24:00 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,8 +12,7 @@ def input_to_index(user_input = [1,2,3,4,5,6,7,8,9])
 end
 
 
-def move(board)
-  def update_array_at_with(board, index, current_player)
+def move(board, index, current_player)
     array[index] = value
   end
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
