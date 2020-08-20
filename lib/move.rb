ea92954751c74e370b07626470d38cd65163b376
Khaled S Momani <khaledmomani4@gmail.com>
2020-08-20 23:35:25 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,7 +12,7 @@ def input_to_index(user_input = [1,2,3,4,5,6,7,8,9])
 end
 
 
-def move(board, index, current_player)
+def move(board, index, current_player = "X")
     board [index] = current_player
   end
   
