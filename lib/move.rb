cf6acd98ec71c8d1fa076fcfe9bd045c35fdae9f
Khaled S Momani <khaledmomani4@gmail.com>
2020-08-20 23:24:10 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -15,11 +15,4 @@ end
 def move(board, index, current_player)
     array[index] = value
   end
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-  
   
