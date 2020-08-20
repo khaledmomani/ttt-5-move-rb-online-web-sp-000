7688b11e3a0e5b0733eaea9f653aa713398224c0
Khaled S Momani <khaledmomani4@gmail.com>
2020-08-20 23:19:45 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,8 +12,9 @@ def input_to_index(user_input = [1,2,3,4,5,6,7,8,9])
 end
 
 
-def move(X, O, board[])
-  
+def move(X, O, board)
+  def update_array_at_with(array, index, value)
+    array[index] = value
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
