b5b55499b2d510369ca3f1dfb006962030c57c40
Khaled S Momani <khaledmomani4@gmail.com>
2020-08-20 23:13:08 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,6 +12,7 @@ def input_to_index(user_input = [1,2,3,4,5,6,7,8,9])
 end
 
 def move(X, O, board = ["1","2","3","4","5","6","7","8","9"])
+  
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
