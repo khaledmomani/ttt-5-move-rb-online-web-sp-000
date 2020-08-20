f73a0bdcd3aa93f71e416d4f2956f74c7d8078b0
Khaled S Momani <khaledmomani4@gmail.com>
2020-08-20 22:21:03 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,11 +8,5 @@ end
 
 def input_to_index(number, board)
   number.to_i= board
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-  
 end
   
