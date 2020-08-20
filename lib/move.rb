15483be890c9047130fa649a24419328fba8facb
Khaled S Momani <khaledmomani4@gmail.com>
2020-08-20 22:00:26 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,4 @@
-def display_board(board)
+def display_board(board= [1,2,3,4,5,6,7,8,9])
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
